7bac07b082ee6b4443d6cbb5ae2202b6828a24a6
dgTr3 <Edugta31@gmail.com>
2026-02-24 16:14:57 -0600

    pide el monto de la venta

--- Ejercicio22/Program.cs
+++ Ejercicio22/Program.cs
@@ -1,1 +1,8 @@
-﻿Console.WriteLine("");
+﻿Console.WriteLine("Cuantas ventas va a ingresar");
+int n=int.Parse(Console.ReadLine());
+double ventas=0;
+for(int i=0;i<n;i++){
+    Console.WriteLine("Ingrese el monto de la venta");
+    ventas+=double.Parse(Console.ReadLine());
+}
+
